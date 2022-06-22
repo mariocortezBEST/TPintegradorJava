d66b1e97e041a4043fb3bc6313e1cddb7f122682
mario cortez <mrcortez566@gmail.com>
2022-06-22 11:29:04 -0300

    version inicial cinemar

---
+++ Registro.java
@@ -0,0 +1,10 @@
+package cinemar;
+
+public class Registro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("esta es la primer linea de codigo");
+	}
+
+}
