37f2fcce17cf2de7288cb072ca1578be2a825ae8
Juan Llatser <juanllatser@hotmail.com>
2022-07-05 22:51:36 -0300

    comit de juan

--- src/cinemar/Registro.java
+++ src/cinemar/Registro.java
@@ -5,6 +5,7 @@ public class Registro {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("esta es la primer linea de codigo");
+      System.out.println("esta es la segunda linea de codigo");
 	}
 
 }
